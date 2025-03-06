5f8680bf5fc3e870feaf8fb9eb29860508b9aaca
Mathieu HERNANDEZ <hernandez.mathieu19@gmail.com>
2025-03-06 08:18:10 +0100

    changed projects order

--- nextjs-blog/components/ProjectGrid.tsx
+++ nextjs-blog/components/ProjectGrid.tsx
@@ -6,37 +6,7 @@ import { DiNetbeans } from "react-icons/di";
 const ProjectGrid = () => {
   return (
     <div className={styles.projectsGrid}>
-      <ProjectCard title="Finder" description="Application web de prestations de services entre particuliers" previewImage="/finder-preview.png
-      " width="320px" height="280px">
-          <div className={styles.techStack}>
-            <div className={styles.techBadge}><SiLaravel />Laravel</div>
-            <div className={styles.techBadge}><SiPhp /> PHP</div>
-            <div className={styles.techBadge}><SiDocker /> Docker</div>
-          </div>
-          <div className={styles.modalContentGrid}>
-            <img src="/finder-2.png" alt="Finder - page d'accueil" />
-            <section>
-              <p>
-                Dans le cadre de ma 2ème année de BUT Informatique, nous devions réaliser un projet en groupe, à présenter à la fin du 
-                second semestre. Les différents sujets étaient répartis, avec pour chacun, un commanditaire (professeur de l'IUT). <br/>
-                Notre projet était Finder, application de services entre prestataires. Cette application permet de publier des annonces 
-                de services doit vous être rendu (ex: ménage, jardinage, cours particuliers, etc.) et de répondre à des annonces, 
-                pour communiquer avec la personne en proposant son prix. Il y a également un système de notation et de commentaires.
-              </p>
-            </section>
-            <section>
-              <p>
-                Nous avons choisi de réaliser ce projet en PHP, avec Docker pour contenairiser la base de données et le serveur web.<br/>
-                Pour faire évoluer l'application, nous l'avons amélioré en utilisant Laravel, un framework PHP. <br/>
-                J'ai pu apprendre à travailler en équipe, à gérer un projet en mettant en oeuvre des méthodes agiles, à utiliser des outils de versionning (Git) et à
-                travailler avec des technologies que je ne connaissais pas. <br/>
-                En 3ème année, nous avons pu revenir sur ce projet pour une simulation d'entreprenariat, en calculant un business plan.
-              </p>
-            </section>
-            <img src="/finder-1.png" alt="Finder - recherche d'annonces" />
-
-          </div>
-      </ProjectCard>
+     
 
 
       <ProjectCard
@@ -79,6 +49,33 @@ const ProjectGrid = () => {
         </div>
       </ProjectCard>
 
+      
+      <ProjectCard title="Tosser of Coin" description="API Rest - Jeu vidéo sans interface de pile ou face amélioré" previewImage="/tosserofcoin-preview.png"  width="320px" height="280px">
+          <div className={styles.techStack}>
+            <div className={styles.techBadge}><FaNodeJs />Node.js</div>
+            <div className={styles.techBadge}><SiExpress />ExpressJs</div>
+            <div className={styles.techBadge}><SiTypescript />TypeScript</div>
+          </div>
+          <div className={styles.modalContentGrid}>
+            <img src="/tosserofcoin-1.png" alt="Requête pour jouer à Tosser of Coin" />
+            <section>
+              <p>
+                Dans le cadre de ma 2ème année de BUT Informatique,nous devions créer une API Rest en Node.js, avec Express, en TypeScript. <br/>
+                Afin de sortir du lot et de créer un projet plus intéressant, j'ai décidé de créer une API pour un jeu vidéo sans interface de pile ou face amélioré. <br/>
+                L'API permet de créer un compte, de se connecter, de jouer, de consulter le classement des joueurs, et de consulter son inventaire.
+                Les administrateurs peuvent bannir des joueurs, mais aussi créer des objets et en rajouter aux inventaires des joueurs. <br/>
+                Le jeu est uniquement accessible via des requêtes HTTP, et permet de jouer à pile ou face, mais avec des règles supplémentaires. <br/>
+                Des objets sont présents, et permettent de manipuler les lancers de pièces (Les plombs permettent de manipuler le poix d'un côté, 
+                les ressorts permettent de survivre en cas de défaite, certains pièces ont des probabilités différentes et donnent ou retirent plus de points.)
+                Le jeu contient également des ennemis, qui peuvent détruire des objets, et qu'il faut éliminer en les plaçant sur la pièce. <br/>
+                Ce projet m'a permis de développement mes compétences et connaissances sur les API Rest, de mieux comprendre le fonctionnement des middlewares, des DAO 
+                (Data Access Object) et des DTO (Data Transfer Object).
+              </p>
+            </section>
+          </div>
+          
+      </ProjectCard>
+
       <ProjectCard title="GLPROJECT" description="Projet pédagogique présentant des fonctionnalités de la librairie OpenGL" previewImage="/opengl.jpg"  width="320px" height="280px">
           <div className={styles.techStack}>
             <div className={styles.techBadge}><SiCplusplus />C++</div>
@@ -98,6 +95,38 @@ const ProjectGrid = () => {
                  <br/>
               </p>
             </section>
+
+          </div>
+      </ProjectCard>
+
+      <ProjectCard title="Finder" description="Application web de prestations de services entre particuliers" previewImage="/finder-preview.png
+      " width="320px" height="280px">
+          <div className={styles.techStack}>
+            <div className={styles.techBadge}><SiLaravel />Laravel</div>
+            <div className={styles.techBadge}><SiPhp /> PHP</div>
+            <div className={styles.techBadge}><SiDocker /> Docker</div>
+          </div>
+          <div className={styles.modalContentGrid}>
+            <img src="/finder-2.png" alt="Finder - page d'accueil" />
+            <section>
+              <p>
+                Dans le cadre de ma 2ème année de BUT Informatique, nous devions réaliser un projet en groupe, à présenter à la fin du 
+                second semestre. Les différents sujets étaient répartis, avec pour chacun, un commanditaire (professeur de l'IUT). <br/>
+                Notre projet était Finder, application de services entre prestataires. Cette application permet de publier des annonces 
+                de services doit vous être rendu (ex: ménage, jardinage, cours particuliers, etc.) et de répondre à des annonces, 
+                pour communiquer avec la personne en proposant son prix. Il y a également un système de notation et de commentaires.
+              </p>
+            </section>
+            <section>
+              <p>
+                Nous avons choisi de réaliser ce projet en PHP, avec Docker pour contenairiser la base de données et le serveur web.<br/>
+                Pour faire évoluer l'application, nous l'avons amélioré en utilisant Laravel, un framework PHP. <br/>
+                J'ai pu apprendre à travailler en équipe, à gérer un projet en mettant en oeuvre des méthodes agiles, à utiliser des outils de versionning (Git) et à
+                travailler avec des technologies que je ne connaissais pas. <br/>
+                En 3ème année, nous avons pu revenir sur ce projet pour une simulation d'entreprenariat, en calculant un business plan.
+              </p>
+            </section>
+            <img src="/finder-1.png" alt="Finder - recherche d'annonces" />
 
           </div>
       </ProjectCard>
@@ -154,31 +183,6 @@ const ProjectGrid = () => {
           </div>
       </ProjectCard>
 
-      <ProjectCard title="Tosser of Coin" description="API Rest - Jeu vidéo sans interface de pile ou face amélioré" previewImage="/tosserofcoin-preview.png"  width="320px" height="280px">
-          <div className={styles.techStack}>
-            <div className={styles.techBadge}><FaNodeJs />Node.js</div>
-            <div className={styles.techBadge}><SiExpress />ExpressJs</div>
-            <div className={styles.techBadge}><SiTypescript />TypeScript</div>
-          </div>
-          <div className={styles.modalContentGrid}>
-            <img src="/tosserofcoin-1.png" alt="Requête pour jouer à Tosser of Coin" />
-            <section>
-              <p>
-                Dans le cadre de ma 2ème année de BUT Informatique,nous devions créer une API Rest en Node.js, avec Express, en TypeScript. <br/>
-                Afin de sortir du lot et de créer un projet plus intéressant, j'ai décidé de créer une API pour un jeu vidéo sans interface de pile ou face amélioré. <br/>
-                L'API permet de créer un compte, de se connecter, de jouer, de consulter le classement des joueurs, et de consulter son inventaire.
-                Les administrateurs peuvent bannir des joueurs, mais aussi créer des objets et en rajouter aux inventaires des joueurs. <br/>
-                Le jeu est uniquement accessible via des requêtes HTTP, et permet de jouer à pile ou face, mais avec des règles supplémentaires. <br/>
-                Des objets sont présents, et permettent de manipuler les lancers de pièces (Les plombs permettent de manipuler le poix d'un côté, 
-                les ressorts permettent de survivre en cas de défaite, certains pièces ont des probabilités différentes et donnent ou retirent plus de points.)
-                Le jeu contient également des ennemis, qui peuvent détruire des objets, et qu'il faut éliminer en les plaçant sur la pièce. <br/>
-                Ce projet m'a permis de développement mes compétences et connaissances sur les API Rest, de mieux comprendre le fonctionnement des middlewares, des DAO 
-                (Data Access Object) et des DTO (Data Transfer Object).
-              </p>
-            </section>
-          </div>
-          
-      </ProjectCard>
 
       <ProjectCard title="PWDManager" description="Application de gestion de mots de passe en Java" previewImage="/pwdmanager-preview.png"  width="320px" height="280px">
           <div className={styles.techStack}>
