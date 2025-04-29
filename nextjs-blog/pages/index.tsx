bd36193119908b6af62a5e2e1ddf185063a37bef
Mathieu HERNANDEZ <hernandez.mathieu19@gmail.com>
2025-04-29 16:00:56 +0200

    commented duplicate information in About Me

--- nextjs-blog/pages/index.tsx
+++ nextjs-blog/pages/index.tsx
@@ -127,9 +127,9 @@ export default function Home() {
               <h2 className={styles.aboutMeTitle}>À propos de moi</h2>
               <section className={styles.aboutMeDescription}>
                 
-              Je m'appelle <b>Mathieu HERNANDEZ</b>, j'ai 20 ans et j'intègre bientôt <b>Epitech</b>, Marseille, dans le cadre d'un <b>Master of Science Technique</b>, avec une spécialisation en <b>IA</b>, après avoir réalisé un <b>BUT Informatique</b> à l'IUT d'Arles. <br/>
-              Passionné par la programmation et souhaitant m'orienter vers l’<b>intelligence artificielle</b> et y faire carrière,
-              je vais bientôt intégrer un Master of Science en IA à <b>Epitech</b>.<br/>
+              Je m'appelle <b>Mathieu HERNANDEZ</b>, j'ai 20 ans et j'intègre bientôt <b>Epitech</b>, Marseille, dans le cadre d'un <b>Master of Science Technique</b>, avec une spécialisation en <b>Intelligence Artificielle</b>, après avoir réalisé un <b>BUT Informatique</b> à l'IUT d'Arles. <br/>
+              {/* Passionné par la programmation et souhaitant m'orienter vers l’<b>intelligence artificielle</b> et y faire carrière,
+              je vais bientôt intégrer un Master of Science en IA à <b>Epitech</b>.<br/> */}
               Grâce à ma formation et mes expériences professionnelles, j’ai acquis de solides compétences en <b>développement web</b>,
               en optimisation des performances, ainsi qu’en <b>gestion de projet</b> et qualité de développement. <br/>
               Ces expériences m’ont permis d’adopter des méthodologies <b>rigoureuses</b> et d’améliorer ma capacité à concevoir des solutions <b>efficaces</b> et bien <b>structurées</b>.
