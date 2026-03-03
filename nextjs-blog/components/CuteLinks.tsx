fc277d529e3583ce6bba2d320e8e7d30ce2aa043
Mathieu HERNANDEZ <hernandez.mathieu19@gmail.com>
2026-03-03 14:56:41 +0100

    fix: better cutelinks gap on mobile

--- nextjs-blog/components/CuteLinks.tsx
+++ nextjs-blog/components/CuteLinks.tsx
@@ -30,7 +30,7 @@ export default function CuteLinks() {
     );
 
     return (
-    <div className="w-full max-w-[95vw] md:max-w-4xl mx-auto grid grid-cols-4 place-items-center md:flex md:flex-row md:flex-wrap md:items-center md:justify-start gap-2 pt-4 px-4 md:px-0">
+        <div className="w-full max-w-[95vw] md:max-w-4xl mx-auto flex flex-wrap justify-center md:justify-start items-center gap-2 pt-4 px-4 md:px-0">
           <CuteLink href="/chat" src="/thameiu.webp" alt="chat" borderColor="var(--color-accent)" />
           <CuteLink href="https://www.axelmanguian.fr" src="/axelmanguian.webp" alt="Axel Manguian" borderColor="#066767" />
           <CuteLink href="https://ergosix-rc.vercel.app/" src="/ergosix.webp" alt="Ergosix Recrutement" borderColor="#3D6D2D" />
@@ -40,6 +40,7 @@ export default function CuteLinks() {
           <CuteLink href="https://in.tern.et/products/winrar-archive-messenger-bag-prod" src="https://cyber.dabamos.de/88x31/winrar4.gif" alt="Winrar Bag" borderColor="#" />
           <CuteLink href="https://dyskinesiaa.com/" src="/dyskinesiaa.webp" alt="Check out dyskinesiaa" borderColor="#000000" />
           <CuteLink href="" src="https://cyber.dabamos.de/88x31/rainbow_bev.gif" alt="Love is love" borderColor="#" />
+          {/* TODO : R4 (bag),  */}
       </div>
     )
 }
