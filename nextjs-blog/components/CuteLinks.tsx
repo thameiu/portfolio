966ff73679f64f4d3ae82ab3db3dfe63156279fd
Mathieu HERNANDEZ <hernandez.mathieu19@gmail.com>
2026-03-03 13:17:02 +0100

    feat: add ergosix, winrar, pdf24, dyskinesiaa cutelinks

--- nextjs-blog/components/CuteLinks.tsx
+++ nextjs-blog/components/CuteLinks.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 export default function CuteLinks() {
 
     const CuteLink = ({ href, src, alt, borderColor }: { href: string; src: string; alt: string , borderColor:string}) => (
-        <Link href={href} target="_blank" rel="noopener noreferrer" className='transition-all hover:-translate-y-1'>
-            <Image 
+        <Link href={href} target="_blank" rel="noopener noreferrer" className='transition-all md:hover:-translate-y-1'>
+            <img 
             src={src} 
             alt={alt} 
             width={80} 
@@ -23,6 +23,10 @@ export default function CuteLinks() {
       <div className="w-full max-w-[95vw] md:max-w-4xl flex flex-row items-center justify-start gap-2 pt-4 px-4 md:px-0">
         <CuteLink href="/chat" src="/badge.webp" alt="chat" borderColor="var(--color-accent)" />
         <CuteLink href="https://www.axelmanguian.fr" src="/axelmanguian.webp" alt="Axel Manguian" borderColor="#066767" />
+        <CuteLink href="https://ergosix-rc.vercel.app/" src="/ergosix.webp" alt="Ergosix Recrutement" borderColor="#3D6D2D" />
+        <CuteLink href="https://in.tern.et/products/winrar-archive-messenger-bag-prod" src="https://cyber.dabamos.de/88x31/winrar4.gif" alt="Winrar Bag" borderColor="#" />
+        <CuteLink href="https://www.pdf24.org/fr/" src="/pdf24.webp" alt="Download PDF24" borderColor="#67B1FF" />
+        <CuteLink href="https://dyskinesiaa.com/" src="/dyskinesiaa.webp" alt="Check out dyskinesiaa" borderColor="#000000" />
  
       </div>
     )
