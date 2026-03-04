18561ae2c874f01f64ea7a2b6b7649278030eeef
Mathieu HERNANDEZ <hernandez.mathieu19@gmail.com>
2026-03-04 14:17:27 +0100

    fix : removed borders idkkkkk

--- nextjs-blog/components/CuteLinks.tsx
+++ nextjs-blog/components/CuteLinks.tsx
@@ -10,7 +10,7 @@ export default function CuteLinks() {
             src={src} 
             alt={alt} 
             width={80} 
-            height={20} 
+            height={31} 
             className='transition-all rounded border w-[68px] h-auto md:w-[80px] md:h-5'
             style={{ borderColor: borderColor || 'transparent' }}
             />
@@ -39,12 +39,11 @@ export default function CuteLinks() {
           <CuteLink href="https://github.com/timothygebhard/js-colormaps" src="/jscolormaps.webp" alt="JS Colormaps by timothygebhard on GitHub" />
           <CuteLink href="https://www.pdf24.org/fr/" src="/pdf24.webp" alt="Download PDF24" borderColor="#67B1FF" />
           <CuteLink href="https://temp-mail.org/" src="/tempmail.webp" alt="Temp Mail" borderColor="#00C497" />
-          <CuteLink href="https://bitoduc.fr/" src="/bitoduc.gif" alt="Bitoduc.fr, référence littéraire pour développeurs" borderColor="#" />
-          <CuteLink href="https://in.tern.et/products/winrar-archive-messenger-bag-prod" src="https://cyber.dabamos.de/88x31/winrar4.gif" alt="Cool Winrar Bag" borderColor="#" />
+          <CuteLink href="https://bitoduc.fr/" src="/bitoduc.gif" alt="Bitoduc.fr, référence littéraire pour développeurs"/>
+          <CuteLink href="https://in.tern.et/products/winrar-archive-messenger-bag-prod" src="https://cyber.dabamos.de/88x31/winrar4.gif" alt="Cool Winrar Bag"  />
           <CuteLink href="https://in.tern.et/products/r4-messenger-bag-tern%C2%AE-bundle" src="/R4.webp" alt="Very Cool R4 Bag" borderColor="#AAAAAA" />
           {/* <CuteLink href="https://davrilsupply.com/" src="/davrilsupply.webp" alt="DAVRILSUPPLY" borderColor="#FFFFFF" /> */}
           <CuteLink href="https://dyskinesiaa.com/" src="/dyskinesiaa.webp" alt="Check out dyskinesiaa" borderColor="#000000" />
-          <CuteLink src="https://cyber.dabamos.de/88x31/hasmile.gif" alt="Smile !"/>
           <CuteLink src="https://cyber.dabamos.de/88x31/rainbow_bev.gif" alt="Love is love" />
           {/* TODO : EZGif, Ile.a.gammes, Bruno */}
       </div>
