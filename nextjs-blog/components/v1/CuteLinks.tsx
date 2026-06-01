bcc9e444297d8a23b41cece803871519219fc880
Mathieu HERNANDEZ <hernandez.mathieu19@gmail.com>
2026-06-01 10:01:19 +0200

    feat: zed gif

--- nextjs-blog/components/v1/CuteLinks.tsx
+++ nextjs-blog/components/v1/CuteLinks.tsx
@@ -13,7 +13,7 @@ export default function CuteLinks({longer=false}) {
               height={31}
               quality={55}
               sizes="(max-width: 768px) 68px, 80px"
-              className='transition-all rounded border w-[68px] h-auto md:w-[80px] md:h-5'
+              className='transition-all  border w-[68px] h-auto md:w-[80px] md:h-5'
               style={{ borderColor: borderColor || 'transparent' }}
             />
         </Link>
@@ -26,7 +26,7 @@ export default function CuteLinks({longer=false}) {
               height={20}
               quality={55}
               sizes="(max-width: 768px) 68px, 80px"
-              className='transition-all rounded w-[68px] h-auto md:w-[80px] md:h-5'
+              className='transition-all border w-[68px] h-auto md:w-[80px] md:h-5'
               style={{ borderColor: borderColor || 'transparent' }}
             />
         </div>
@@ -39,10 +39,8 @@ export default function CuteLinks({longer=false}) {
           <CuteLink href="https://16ur.vercel.app" src="/16ur.gif" alt="Axel Manguian" borderColor="#066767" />
           <CuteLink href="https://soundcloud.com/mydriax" src="/mydriax.gif" alt="Stream Mydriax" borderColor="#24004C" />
           <CuteLink href="https://ergosix-rc.vercel.app/" src="/ergosix.webp" alt="Ergosix Recrutement" borderColor="#3D6D2D" />
-          <CuteLink href="https://react-icons.github.io/react-icons/" src="/react-icons.gif" alt="React Icons" borderColor="#E91E63" />
-          <CuteLink href="https://github.com/timothygebhard/js-colormaps" src="/jscolormaps.webp" alt="JS Colormaps by timothygebhard on GitHub" />
-
-          <CuteLink href="https://in.tern.et/products/winrar-archive-messenger-bag-prod" src="https://cyber.dabamos.de/88x31/winrar4.gif" alt="Cool Winrar Bag"  />
+          {/*<CuteLink href="https://github.com/timothygebhard/js-colormaps" src="/jscolormaps.webp" alt="JS Colormaps by timothygebhard on GitHub" />*/}
+          <CuteLink href="https://zed.dev" src="/zed.gif" alt="Zed IDE supremacy" borderColor="#5b86e9" />
           <CuteLink href="https://in.tern.et/products/r4-messenger-bag-tern%C2%AE-bundle" src="/R4.webp" alt="Very Cool R4 Bag" borderColor="#AAAAAA" />
 
           <CuteLink src="https://cyber.dabamos.de/88x31/hasmile.gif" alt="Smile !" />
